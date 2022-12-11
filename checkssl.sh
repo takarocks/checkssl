41f0d85d6f96f0652a0092512a762665047446e4
Taka K <35846685+takarocks@users.noreply.github.com>
2022-12-11 13:10:57 -0500

    Update checkssl.sh

--- checkssl.sh
+++ checkssl.sh
@@ -1,9 +1,9 @@
-function checkssl(){
-  if [ -z "$1" ];then
-    printf "Specify the target domain to check SSL\n"
-  else
-    # Uncomment if you want to show actual command
-    # printf "echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | head\n"
-    echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | head
-  fi
-}
+#!/bin/zsh
+
+if [ -z "$1" ];then
+  printf "Specify the target domain to check SSL\n"
+else
+  # Uncomment if you want to show actual command
+  # printf "echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | head\n"
+  echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | openssl x509 -inform pem -noout -text | head
+fi
